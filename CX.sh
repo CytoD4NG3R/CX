ea1e42e0448459e2068c57a2be395a11906861e0
CytoXploit <51657151+CytoD4NG3R@users.noreply.github.com>
2019-06-11 00:06:53 +0700

    CX
    Pip install requests
    Pip install mechanize
    Pip install --upgrade pip

---
+++ CX.sh
@@ -0,0 +1,136 @@
+clear
+bi='\033[34;1m'
+i='\033[32;1m'
+pur='\033[35;1m'
+cy='\033[36;1m'
+me='\033[31;1m'
+pu='\033[37;1m'
+ku='\033[33;1m'
+sleep 2
+echo $cy "[+]===============================================[+]
+  #                                                 # 
+  #"....................$pu"Cyto"$me"X"$pu"ploit"$cy"...................#
+  #                                                 #
+  #=================================================#
+  #                                                 #
+  #    CreadBy  : Mr.D4NG3R                         #
+  #                                                 #
+  #    Contac   : http://cytoxploit.tech            #
+  #                                                 #
+  #    ThanksTo : [Ucuksngek]~[Mr.PanjuL21]         #
+  #               [Mr.p3rc1l]~[r00t_us3r]           #
+  #                                                 #
+  #=================================================#
+  #                                                 #
+  #....................."$pu"THE"$me"END"$cy"......................#
+  #                                                 #
+ [+]===============================================[+]"
+  sleep 1
+  read -p "Tekan y untuk melanjutkan, Jomblo Ya?  y/n:" pill
+  
+  if [ $pill = y ]; then
+  figlet JONES!! | lolcat
+  fi 
+  
+  sleep 1
+ echo $ku"======================
+# [1] DDOS HAMMER    #
+======================"
+sleep 0.5
+ echo $bi"======================
+# [2] DDOS XERXES    #
+======================"
+sleep 0.5
+ echo $cy"======================
+# [3] MBF            #
+======================"
+sleep 0.5
+ echo $ku"======================
+# [4] OSIF           #
+======================"
+sleep 0.5
+ echo $bi"======================
+# [5] Scaner Virus   #
+======================"
+sleep 0.5
+ echo $cy"======================
+# [6] Report FB      #
+======================"
+sleep 0.5
+ echo $ku"======================
+# [7] Saycheese      #
+======================"
+sleep 0.5
+ echo $bi"======================
+# [8] AutoMBF        #
+======================"
+sleep 0.5
+ echo $cy"======================
+# [9] Brutal SmS     #
+================================"
+ echo $me"Pilih Nomor Boedjank  :  "
+ read nomor
+ if [ $nomor = 1 ] || [ $nomor] 
+ then
+ pkg install python2 git
+ git clone https://github.com/cyweb/hammer
+ cd hammer
+ python hammer.py
+ fi
+ if [ $nomor = 2 ] || [ $nomor] 
+ then
+ apt install clang git
+ git clone https://github.com/zanyarjamal/xerxes.git
+ cd xerxes
+ clang xerxes.c -o xerxes
+ fi
+ if [ $nomor = 3 ] || [ $nomor] 
+ then
+ pkg install python2 git
+ git clone https://github.com/pirmansx/mbf/
+ cd mbf
+ python2 MBF.py
+ fi
+ if [ $nomor = 4 ] || [ $nomor] 
+ then
+ pkg install python2 git
+ git clone https://github.com/CiKu370/OSIF
+ cd OSIF
+ python2 OSIF.py
+ fi
+ if [ $nomor = 5 ] || [ $nomor] 
+ then
+ pkg install git
+ git clone https://github.com/RikiOktopan1/ScannerVirus
+ cd ScannerVirus
+ chmod +x Clean.sh
+ sh Clean.sh
+ fi
+ if [ $nomor = 6 ] || [ $nomor] 
+ then
+ pkg install python2 git
+ git clone https://github.com/PangeranAlvins/Repot3
+ cd Repot3
+ bash install.sh
+ python2 Repot3.py
+ fi
+ if [ $nomor = 7 ] || [ $nomor] 
+ then
+ pkg install python2 
+ git clone https://github.com/thelinuxchoice/saycheese
+ cd saycheese
+ bash saycheese.sh
+ fi
+ if [ $nomor = 8 ] || [ $nomor] 
+ then
+ apt install python2
+ python2 run.py
+ fi
+ if [ $nomor = 9 ] || [ $nomor] 
+ then
+ pkg update && pkg upgrade
+ pkg install git python2
+ git clone https://github.com/TERMUXID3/brutal-sms
+ cd brutal-sms
+ python2 run.py
+ fi
